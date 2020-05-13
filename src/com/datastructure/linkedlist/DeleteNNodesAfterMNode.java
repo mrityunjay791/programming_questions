2c2650da718de62dca090c948a1436d2a22cf57c
mrityunjay kumar <mk710949@gmail.com>
2020-05-13 08:08:14 +0530

    Added Program to delete N node after M-th Node in linked list.

---
+++ src/com/datastructure/linkedlist/DeleteNNodesAfterMNode.java
@@ -0,0 +1,98 @@
+package com.datastructure.linkedlist;
+
+public class DeleteNNodesAfterMNode {
+	static Node head;
+
+	/**
+	 * Add node.
+	 * 
+	 * @param data
+	 */
+	public static void addNode(int data) {
+		if(head == null) {
+			head = new Node(data);
+			return;
+		}
+		
+		Node temp = head;
+		while(temp.next != null) {
+			temp = temp.next;
+		}
+		
+		temp.next = new Node(data);
+	}
+	
+	/**
+	 * Display Node
+	 * 
+	 */
+	public static void printNode() {
+		if(head == null) {
+			System.out.println("Empty list !");
+			return;
+		}
+		
+		Node temp = head;
+		
+		while(temp.next != null) {
+			System.out.print(temp.data+" -> ");
+			temp = temp.next;
+		}
+		
+		System.out.print(temp.data);
+	}
+	
+	public static void deleteNNodeAfterMNode(int m , int n) {
+		if(head == null) {
+			System.out.println("Empty list !! ");
+			return;
+		}
+		Node curr = head;
+		while(curr != null) {
+		
+		
+			int tempM = m;
+			while(tempM > 1 && curr != null) {
+				curr = curr.next;
+				tempM--;
+			}
+			
+			
+			if(curr == null) 
+				return;
+			
+			int tempN = n;
+			while(tempN > 0 && curr.next != null) {
+				tempN--;
+				curr.next = curr.next.next;
+			}
+			
+			curr = curr.next;
+			
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		addNode(1);
+		addNode(2);
+		addNode(3);
+		addNode(4);
+		addNode(5);
+		addNode(6);
+		addNode(7);
+		addNode(8);
+		addNode(9);
+		addNode(10);
+		addNode(11);
+	
+		int m = 3, n = 2;
+		System.out.println("Linked list nodes: ");
+		printNode();
+		
+		System.out.println("\nAfter modification: ");
+		deleteNNodeAfterMNode(m, n);
+		printNode();
+	}
+
+}
